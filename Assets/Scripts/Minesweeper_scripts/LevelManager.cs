dcbddcca8f37533cbdbf4ecd8f14231be1c3c9da
GerardoRmzS <gerardors192@gmail.com>
2023-08-10 17:10:04 -0600

    Se agrega el proyceto

---
+++ Assets/Scripts/Minesweeper_scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public void StarGame()
+	{
+		SceneManager.LoadScene("Minesweeper");
+	}
+	
+	public void GotoMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+	
+	public void QuitGame()
+	{
+		Application.Quit();
+		Debug.Log("Quit!");
+	}
+	
+}
